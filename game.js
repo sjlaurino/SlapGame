ebabf90cb1c70f6c10ec02cbf8f9b8c7b6c9cf04
Steven Laurino <sjlaurino@gmail.com>
2019-02-08 14:34:43 -0700

    slap and counter works live refresh

--- game.js
+++ game.js
@@ -1,9 +1,14 @@
 let health = 100
+
 function slap() {
-  for (let i = 100; i >= 0; i--
-  );
-  return health--
+  health--
   console.log(health)
+  update();
 }
+
+function update() {
+  document.getElementById('enemyHealth').innerHTML = 'Health: ' + health.toString();
+}
+
 
 
