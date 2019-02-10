b5230b515516348a880a1337e5ad2a0fc6648c99
Steven Laurino <sjlaurino@gmail.com>
2019-02-10 16:17:49 -0700

    all working minus alert after 0

--- game.js
+++ game.js
@@ -1,8 +1,15 @@
-let target = {
-  health: 100,
-  name: 'Charlie Brown',
-  hits: 0,
-  items: []
+let targets = {
+  charlie: {
+    health: 100,
+    name: 'Charlie Brown',
+    hits: 0,
+    items: [],
+  },
+  lucy: {
+    health: 100,
+    name: 'Lucy van Pelt',
+    hits: 0,
+  }
 }
 
 let items = {
@@ -25,73 +32,104 @@ let items = {
 
 function addMods2() {
   let modTotal = 0;
-  for (let i = 0; i < target.items.length; i++) {
-    modTotal += target.items[i].modifier;
+  for (let i = 0; i < targets.charlie.items.length; i++) {
+    modTotal += targets.charlie.items[i].modifier;
   }
   return modTotal
 }
 
 function slap() {
-  target.health -= 1 + addMods2()
+  targets.charlie.health -= 1 + addMods2()
+  targets.charlie.hits++
+  charlieAttack()
   stopHealth()
-  target.hits++
   update();
-  target.items = [];
 }
 
 function punch() {
-  target.health -= 5 + addMods2()
+  targets.charlie.health -= 5 + addMods2()
+  targets.charlie.hits++
+  charlieAttack()
   stopHealth()
-  target.hits++
   update();
-  target.items = [];
 }
 
 function kick() {
-  target.health -= 10 + addMods2()
+  targets.charlie.health -= 10 + addMods2()
+  targets.charlie.hits++
+  charlieAttack()
   stopHealth()
-  target.hits++
-  update();
-  target.items = [];
+  update()
 }
 function kite() {
-  target.health += 5
+  targets.charlie.health += 5
   stopHealth()
   update();
-  target.items = [];
 }
 function update() {
-  document.getElementById('health').innerHTML = 'Health: ' + target.health.toString();
-  document.getElementById('hits').innerHTML = 'Hits: ' + target.hits.toString();
-  document.getElementById('progress-number').innerText = target.health.toString();
-  document.getElementById('progress-number').style.width = target.health + '%';
+  showAlert()
+  document.getElementById('health').innerHTML = 'Health: ' + targets.charlie.health.toString();
+  document.getElementById('lucy-health').innerHTML = 'Health: ' + targets.lucy.health.toString();
+  document.getElementById('hits').innerHTML = 'Hits: ' + targets.charlie.hits.toString();
+  document.getElementById('lucy-hits').innerHTML = 'Hits: ' + targets.lucy.hits.toString();
+  document.getElementById('progress-number').innerText = targets.charlie.health.toString();
+  document.getElementById('progress-number').style.width = targets.charlie.health + '%';
+  document.getElementById('progress-lucy').innerText = targets.lucy.health.toString();
+  document.getElementById('progress-lucy').style.width = targets.lucy.health + '%';
+  targets.charlie.items = [];
+
+}
+function stopHealth() {
+  if (targets.charlie.health <= 0) {
+    targets.charlie.health = 0;
+    // document.getElementById('insult').innerHTML = 'I hope you feel good about yourself... \nYou just killed the most beloved childrens character in history.'
+  }
+  if (targets.charlie.health >= 100) {
+    targets.charlie.health = 100
+  }
+  if (targets.lucy.health <= 0)
+    targets.lucy.health = 0;
 }
 
+function showAlert() {
+  if (targets.charlie.health <= 0) {
+    alert('You\'ve defeated Charlie! \nClick "Reset" to play again!');
+  } else if (targets.lucy.health <= 0) {
+    alert('You\'ve been defeated by Charlie! \nClick "Reset" to play again!');
+  }
+}
+
+
+
+
+
+
+
 function giveFootball() {
-  target.items.push(items.football);
+  targets.charlie.items.push(items.football);
 }
 
 function giveLucy() {
-  target.items.push(items.lucy);
+  targets.charlie.items.push(items.lucy);
 }
 function reset() {
-  target.health = 100;
-  target.hits = 0;
+  targets.charlie.health = 100;
+  targets.charlie.hits = 0;
+  targets.lucy.health = 100;
+  targets.lucy.hits = 0;
   update()
 }
 
-function stopHealth() {
-  if (target.health <= 0) {
-    target.health = 0;
-    // document.getElementById('insult').innerHTML = 'I hope you feel good about yourself... \nYou just killed the most beloved childrens character in history.'
-    alert('You\'ve defeated Charlie! \nClick "Reset" to play again!')
-  } else if (target.health >= 100) {
-    target.health = 100;
-  }
+
+
+function charlieAttack() {
+  targets.lucy.health -= Math.floor((Math.random() * 15) + 1)
+  targets.lucy.hits++
 }
 
 
 // XX add function and object in dictionary for lucys deception
 // XX get health to stop at 0 and say youve defeated charlie stop at 100 health
 // style page
-// add progress bar **Bonus
+// XX add progress bar **Bonus
+// bootstrap modal to make custom alert at the
